3e9df3e4de8e7f0cb41628405dfd65660919ce1f
Wannes Rombouts <wapiflapi@gmail.com>
2022-01-12 13:44:17 +0900

    Fix group set-up by passing instance of AppConfig.

--- bhot/transplants/apps.py
+++ bhot/transplants/apps.py
@@ -1,5 +1,20 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 from django.utils.translation import gettext_lazy as _
+
+
+def make_group_for_app(sender, **kwargs):
+    # We can not import those models before this function
+    # is called because apps won't have finished loading.
+    from django.contrib.auth.models import Group, Permission
+    from django.contrib.contenttypes.models import ContentType
+
+    group, _ = Group.objects.get_or_create(name=f"Access to {sender.label}")
+    group.permissions.add(
+        *Permission.objects.filter(
+            content_type__in=ContentType.objects.filter(app_label=sender.label)
+        )
+    )
 
 
 class TransplantsConfig(AppConfig):
@@ -7,4 +22,4 @@ class TransplantsConfig(AppConfig):
     verbose_name = _("Transplants")
 
     def ready(self):
-        import bhot.transplants.signals  # noqa F401
+        post_migrate.connect(make_group_for_app, sender=self)
