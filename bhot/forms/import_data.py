ac14e735b99383411ce09878ac449f740a75c440
Wannes Rombouts <wapiflapi@gmail.com>
2022-01-19 16:43:05 +0900

    Remove everything. Clean slate for rewrite.

--- bhot/forms/import_data.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import FileField, ValidationError
-from wtforms.validators import DataRequired, Optional
-
-class ImportDataForm(FlaskForm):
-    xls = FileField('XLS File', validators=[DataRequired()])
-    rcc = FileField('RCC File', validators=[Optional()])
--- bhot_runner.py
+++
@@ -1,1 +0,0 @@
-from bhot.app import app
