02724a9c75cd503331842403a075efcd50dc2ee7
Wannes Rombouts <wapiflapi@gmail.com>
2022-02-22 21:32:36 +0100

    Add Biopsy.proteinuria as an example of a field with a unit.

--- bhot/transplants/models/biopsy.py
+++ bhot/transplants/models/biopsy.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from bhot.transplants.models.transplant import Transplant
 from bhot.users.models import UserScopedModel
@@ -10,6 +11,21 @@ class Biopsy(UserScopedModel):
     class Meta:
         verbose_name_plural = "biopsies"
 
+    class ProteinuriaUnit(models.TextChoices):
+        GRAM_PER_GRAM = "g/g", _("g/g")
+        GRAM_PER_24H = "g/24h", _("g/24h")
+        GRAM_PER_LITER = "g/L", _("g/L")
+        MILLIGRAM_PER_MILLIMOLE = "mg/mmol", _("mg/mmol")
+        GRAM_PER_MILLIMOLE = "g/mmol", _("g/mmol")
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     transplant = models.ForeignKey(Transplant, null=True, on_delete=models.SET_NULL)
     biopsy_date = models.DateField()
+
+    proteinuria = models.FloatField(blank=True, null=True)
+    proteinuria_unit = models.CharField(
+        max_length=32,
+        choices=ProteinuriaUnit.choices,
+        blank=False,
+        default=ProteinuriaUnit.MILLIGRAM_PER_MILLIMOLE,
+    )
